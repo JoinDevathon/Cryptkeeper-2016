0362cb74258f1a6244d80f75c071de8f41758035
Cryptkeeper <hello@cryptkpr.me>
2016-11-05 12:02:35 -0500

    PIVOT TIME YOLO

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -9,11 +9,6 @@ public class DevathonPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        getServer().getPluginManager().registerEvents(new GenericListener(), this);
-        getServer().getPluginManager().registerEvents(new AnimationListener(), this);
-
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, new MessageTask(), 2L, 2L);
     }
 
     public static DevathonPlugin getInstance() {
